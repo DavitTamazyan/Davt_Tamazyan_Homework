a0e74b73bd07da6d3b92707955dc87b433c94998
DavitTamazyan <52210506+DavitTamazyan@users.noreply.github.com>
2020-10-18 16:37:21 +0400

    Update IsUniq.cpp

--- Lesson_2/IsUniq.cpp
+++ Lesson_2/IsUniq.cpp
@@ -1,61 +1,53 @@
 class bitset {
 public:
 
-	bitset(int init_value) {
-		reg0 = init_value;
-		reg1 = init_value;
-		reg2 = init_value;
-		reg3 = init_value;
+	bitset(int size) {
+		int* table = new int[size];
+		///
+		this->table = table;
+		this->size = size;
+		FillArray(table, size);
 	}
 
-	bool check(char index) {
-		if (index < 64) {
-			return reg0 & (one << index);
-		}
-		else if (index < 128) {
-			return reg1 & (one << (index - 64));
-		}
-		else if (index < 192) {
-			return reg2 & (one << (index - 128));
-		}
-		else if (index < 256) {
-			return reg3 & (one << (index - 192));
-		}
+	~bitset() {
+		delete[] table;
 	}
 
-	void AddElement(char index) {
-		if (index < 64) {
-			reg0 |= (one << index);
-		}
-		else if (index < 128) {
-			reg1 |= (one << (index - 64));
-		}
-		else if (index < 192) {
-			reg2 |= (one << (index - 128));
-		}
-		else if (index < 256) {
-			reg3 |= (one << (index - 192));
-		}
+	bool GetValue(char index) {
+		return table[index];
+	}
+
+	void SetValue(char index) {
+		table[index] = 1;
 	}
 
 private:
-	long long unsigned reg0;
-	long long unsigned reg1;
-	long long unsigned reg2;
-	long long unsigned reg3;
-	long long one = 1;
+	int* table;
+	int size;
+
+	void FillArray(int* arr, int size) {
+		for (int i = 0; i < size; ++i) {
+			arr[i] = 0;
+		}
+	}
+
+	void PrintArray(int* arr, int size) {
+		for (int i = 0; i < size; ++i) {
+			cout << arr[i] << "'";
+		}
+	}
 };
 
 bool IsUniq(string s) {
 	
-	bitset table = 0;
+	bitset table(700);
 
 	for (int i = 0; i < s.size(); ++i) {
-		if (table.check(s[i])) {
+		if (table.GetValue(s[i])) {
 			return false;
 		}
 		else {
-			table.AddElement(s[i]);
+			table.SetValue(s[i]);
 		}
 	}
 	return true;
