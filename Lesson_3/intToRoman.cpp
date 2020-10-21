28ebdd5e9e8202fda4abe0f72f0770144be9f90b
DavitTamazyan <52210506+DavitTamazyan@users.noreply.github.com>
2020-10-21 14:49:31 +0400

    Update intToRoman.cpp

--- Lesson_3/intToRoman.cpp
+++ Lesson_3/intToRoman.cpp
@@ -1,17 +1,10 @@
-#include<iostream>
-#include<string>
-#include<vector>
-
-using namespace std;
-
-
+//Runtime: 4 ms, faster than 96.25% of C++ online submissions for Integer to Roman.
+//Memory Usage: 6.2 MB, less than 11.53% of C++ online submissions for Integer to Roman.
 string intToRoman(int num) {
 	char Roman_numerals[9] = {'*', 'M', 'D', 'C', 'L', 'X', 'V', 'I', '*' };
 	int Roman_numbers[9] = {5000, 1000, 500 ,100 ,50 ,10, 5, 1, 0 };
 	int divider = 1000;
 	string result;
-
-
 
 	for (int i = 1; i < 8; ++i) {
 		cout << num << endl;
@@ -56,16 +49,4 @@ string intToRoman(int num) {
 	}
 
 	return result;
-}
-
-int main() {
-
-	int number;
-
-	//while (1) {
-		cin >> number;
-
-		cout << intToRoman(number) << endl;
-		cout << endl;
-	//}
 }
